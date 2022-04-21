29ef4c6ddc8166d8c5542a7066e072ecf3153a48
Kathryn Wint <92839131+KatWint@users.noreply.github.com>
2022-04-20 21:16:15 -0700

    first commit

---
+++ BackyardNet/backyardnet/src/main/java/com/group/backyardnet/BackyardnetApplication.java
@@ -0,0 +1,13 @@
+package com.group.backyardnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackyardnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackyardnetApplication.class, args);
+	}
+
+}
